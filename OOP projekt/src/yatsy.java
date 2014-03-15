7e88653b5c8625911ba29bbd57d98a747f58ff6c
Matu07 <matu07@hot.ee>
2014-03-15 18:47:07 +0200

    Mõned mõtted ja küsimused üldstruktuuri osas

--- OOP projekt/src/yatsy.java
+++ OOP projekt/src/yatsy.java
@@ -1,13 +1,23 @@
 public class yatsy {
 
-	/**muutsin seda natuke
-	 * selle kirjutasin veebi kaudu juurde
-	 * @param args
-	 */
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("töötab!");
-		System.out.println("Kui see tuleb läbi, siis oskan teha !");
+
+		// System.out.println("töötab!");
+		// System.out.println("Kui see tuleb läbi, siis oskan teha !");
+		System.out.println("Yatzy mäng");
+		System.out.println("Veeretage täringuid!");
+		// Olemas:täringute kujutised
+		// Luua 5 täringu muutujat, mis saavad Randomiga väärtused 1-6, kujutamisel abiks klass "Numbrid"
+		System.out.println("Saite järgmise tulemuse:");
+		// Nüüd on päris mängus võimalik kahel korral valida, milliseid uuesti veeretada. Tuleb kasutajalt küsida sisend.
+		System.out.println("Valige, milliseid täringuid uuesti veeretada:");
+		// Kombinatsioonidega põhiküsimus: kas läbida kindlas järjekorras, lasta kasutajal valida või otsustada programmi sees?
+		// Teha iga kombinatsiooni kontrollmehhanism. Kui ei täida tingimusi, läheb 0. Kuidas hea teostada?
+		// Boonuspunktid esimese osa täieliku täitmise eest?
+		// Lõppskoori arvutamine
+		System.out.println("Teie lõppskoor on:");
+		
 
 	}
 
