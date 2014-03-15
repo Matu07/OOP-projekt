b5e68770a3fcc5883fcdb96465b5484a87a5b86e
silverwithp <silverpajumae@hotmail.com>
2014-03-15 13:44:19 +0200

    New Commit

---
+++ OOP projekt/src/Numbrid.java
@@ -0,0 +1,68 @@
+
+public class Numbrid {
+	
+	 static void Number1(){
+		System.out.println(" ___");
+		System.out.println("|   |");
+		System.out.println("| . |");
+		System.out.println("|   |");
+		System.out.println(" ¯¯¯");
+		return;
+	}
+	 
+	 static void Number2(){
+			System.out.println(" ___");
+			System.out.println("|.  |");
+			System.out.println("|   |");
+			System.out.println("|  .|");
+			System.out.println(" ¯¯¯");
+			return;
+		}
+	 
+	 static void Number3(){
+			System.out.println(" ___");
+			System.out.println("|.  |");
+			System.out.println("| . |");
+			System.out.println("|  .|");
+			System.out.println(" ¯¯¯");
+			return;
+		}
+	 
+	 static void Number4(){
+			System.out.println(" ___");
+			System.out.println("|. .|");
+			System.out.println("|   |");
+			System.out.println("|. .|");
+			System.out.println(" ¯¯¯");
+			return;
+		}
+	 
+	 static void Number5(){
+			System.out.println(" ___");
+			System.out.println("|. .|");
+			System.out.println("| . |");
+			System.out.println("|. .|");
+			System.out.println(" ¯¯¯");
+			return;
+			
+		}
+	 static void Number6(){
+			System.out.println(" ___");
+			System.out.println("|. .|");
+			System.out.println("|. .|");
+			System.out.println("|. .|");
+			System.out.println(" ¯¯¯");
+			return;
+		}
+	
+	 public static void main(String[] args) {
+		Number1();
+		Number2();
+		Number3();
+		Number4();
+		Number5();
+		Number6();
+	}
+	
+
+}
