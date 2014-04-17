b7ba9ffcfe7a754b5a038b9237225773985f5144
silverwithp <silverpajumae@hotmail.com>
2014-04-17 14:21:39 +0300

    Eelmine commit ei tulnud vist läbi

---
+++ OOP projekt2/src/application/Numbrid.java
@@ -0,0 +1,67 @@
+package application;
+
+
+
+public class Numbrid{
+	//Täringute kujutised, mis seotakse väärtustega
+	 public static void Number(int number){
+		 if (number == 1) {
+			System.out.println(" ___");
+			System.out.println("|   |");
+			System.out.println("| . |");
+			System.out.println("|   |");
+			System.out.println(" ¯¯¯"); 
+		 }
+		 else if (number == 2) {
+			System.out.println(" ___");
+			System.out.println("|.  |");
+			System.out.println("|   |");
+			System.out.println("|  .|");
+			System.out.println(" ¯¯¯");
+		 }
+		 else if (number == 3) {
+			System.out.println(" ___");
+			System.out.println("|.  |");
+			System.out.println("| . |");
+			System.out.println("|  .|");
+			System.out.println(" ¯¯¯");
+		 }
+		 else if (number == 4) {
+			System.out.println(" ___");
+			System.out.println("|. .|");
+			System.out.println("|   |");
+			System.out.println("|. .|");
+			System.out.println(" ¯¯¯");
+		 }
+		 else if (number == 5) {
+			System.out.println(" ___");
+			System.out.println("|. .|");
+			System.out.println("| . |");
+			System.out.println("|. .|");
+			System.out.println(" ¯¯¯");
+		 }
+		 else if (number ==6) {
+			System.out.println(" ___");
+			System.out.println("|. .|");
+			System.out.println("|. .|");
+			System.out.println("|. .|");
+			System.out.println(" ¯¯¯");
+		 }
+		 else {
+			 System.out.println("Vale väärtus!!!");
+		 }
+	}
+	 
+	
+	 public static void main(String[] args) {
+		Number(1);
+		Number(2);
+		Number(3);
+		Number(4);
+		Number(5);
+		Number(6);
+		Number(7);
+	}
+	
+
+}
---
+++ OOP projekt2/src/application/Yatzy.java
@@ -0,0 +1,122 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Yatzy extends Application {
+
+	@Override
+	public void start(final Stage PeaMäng) {
+		Group juur = new Group();
+		Text ühed = new Text("Ühed");
+		Text kahed = new Text("Kahed");
+		Text kolmed = new Text("Kolmed");
+		Text neljad = new Text("Neljad");
+		Text viied = new Text("Viied");
+		Text kuued = new Text("Kuued");
+		Text veereta = new Text("Veereta");
+
+		VBox vBox = new VBox(10);
+		vBox.setAlignment(Pos.BASELINE_LEFT);
+		vBox.getChildren().addAll(ühed, kahed, kolmed, neljad, viied, kuued);
+
+		ühed.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent me) {
+				System.out.println("TULEB ÜHTEDE KOGUSUMMA ! ! ! ! ! ! !");
+
+			}
+		});
+
+		// MÄNGIJANIME EI SISESTATUD
+		final Stage mängijaaken = new Stage();
+
+		Label tekst = new Label("TE EI SISESTANUD NIME!");
+		final Stage error = new Stage();
+
+		Button tagasi = new Button("OK. Kohe sisestan.");
+
+		tagasi.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				error.hide();
+				mängijaaken.show();
+			}
+		});
+
+		VBox errorbox = new VBox(10);
+		errorbox.setAlignment(Pos.CENTER);
+
+		FlowPane errorpane = new FlowPane(10, 10);
+		errorpane.setAlignment(Pos.BOTTOM_CENTER);
+		errorpane.getChildren().add(tagasi);
+
+		errorbox.getChildren().addAll(tekst, errorpane);
+
+		Scene errorstseen = new Scene(errorbox);
+		error.setScene(errorstseen);
+
+		error.setTitle("VIGA: EI SISESTATUD NIME!");
+
+		// MÄNGIJANIME AKEN
+
+		final TextField nimi = new TextField();
+		nimi.setText("Sisesta oma nimi");
+
+		Button sisenemängu = new Button("SISENE MÄNGU");
+
+		sisenemängu.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent me) {
+				String mängijanimi = nimi.getText();
+				if (mängijanimi.equals("Sisesta oma nimi")) {
+					mängijaaken.hide();
+					error.show();
+				} else {
+					mängijaaken.hide();
+					PeaMäng.show();
+					System.out.println(mängijanimi);
+				}
+
+			}
+		});
+
+		VBox nimebox = new VBox(10);
+		nimebox.setAlignment(Pos.CENTER);
+
+		FlowPane pane1 = new FlowPane(10, 10);
+		pane1.setAlignment(Pos.BOTTOM_CENTER);
+		pane1.getChildren().add(sisenemängu);
+
+		nimebox.getChildren().addAll(nimi, pane1);
+
+		Scene stseen2 = new Scene(nimebox);
+		mängijaaken.setScene(stseen2);
+
+		mängijaaken.setTitle("MÄNGIJA NIME AKEN");
+
+		Scene stseen1 = new Scene(vBox, 535, 535, Color.GREEN);
+
+		PeaMäng.setTitle("Yatzy");
+
+		PeaMäng.setScene(stseen1);
+
+		PeaMäng.hide();
+		mängijaaken.show();
+		error.hide();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
